e5e17a266a4e4a525f7ee8d30488bb2d8c9eb4ec
Joaquin Rojas Chang <jchang6@hawkmail.hccfl.edu>
2021-02-25 18:57:49 -0500

    controller to make file tertiary work

--- src/main/java/org/jaropeners/PrimaryController.java
+++ src/main/java/org/jaropeners/PrimaryController.java
@@ -1,12 +1,28 @@
 package org.jaropeners;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+
+    public void switchToAbout() throws IOException {
+        App.setRoot("tertiary");
+
+    }
+
+
+    public void switchToPrimary() throws IOException {
+        App.setRoot("primary");
     }
 }
