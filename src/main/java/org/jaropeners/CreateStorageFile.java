99c16c7d84a5b87f7a0d28032c68e98ab572bfb5
Devin McDermott <dc.mcdermott22@gmail.com>
2021-04-03 12:33:01 -0400

    Create storage file created
    Added the CreateStorageFile class to check for the jarOpenersStorage file in the user's home directory and create it if it does not already exist.
    
    Devin McDermott <dc.mcdermott22@gmail.com>

---
+++ src/main/java/org/jaropeners/CreateStorageFile.java
@@ -0,0 +1,31 @@
+package org.jaropeners;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreateStorageFile {
+
+    public static void main(String[] args) {
+        try
+        {
+            String userHome = System.getProperty("user.home");
+            Path storageFile =  Paths.get(userHome, "jarOpenersStorage.txt");
+
+            if (Files.exists(storageFile)) {
+                System.out.println("Storage file already exists");
+            }
+            else {
+                File newStorageFile = new File(userHome, "jarOpenersStorage.txt");
+                BufferedWriter out = new BufferedWriter(new FileWriter(newStorageFile));
+                System.out.println("JarOpenersStorage.txt created successfully");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
